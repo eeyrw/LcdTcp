5011d4cbea0efe58379cb59c2bf5af2774fa14d5
yuan <ayrw@163.com>
2018-02-14 15:55:05 +0800

    Add travis ci related file.

---
+++ travis/common.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function build()
+{
+  # track the exit code for this platform
+  local exit_code=0
+
+    # verify the example, and save stdout & stderr to a variable
+    # we have to avoid reading the exit code of local:
+    # "when declaring a local variable in a function, the local acts as a command in its own right"
+    local build_stdout
+    build_stdout=$(make -f ./makeEspArduino/makeEspArduino.mk -j8 2>&1)
+
+    # echo output if the build failed
+    if [ $? -ne 0 ]; then
+      # heavy X
+      echo -e "\xe2\x9c\x96"
+      echo -e "----------------------------- DEBUG OUTPUT -----------------------------\n"
+      echo "$build_stdout"
+      echo -e "\n------------------------------------------------------------------------\n"
+
+      # mark as fail
+      exit_code=1
+
+    else
+      # heavy checkmark
+      echo -e "\xe2\x9c\x93"
+    fi
+  done
+
+  return $exit_code
+}
