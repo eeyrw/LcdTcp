1f5eb1939e17111faa4359d9226747dd5146f835
Yuan <ayrw@163.com>
2018-02-19 13:10:53 +0800

    Add makeEspArduino as submodule.

--- config.mk
+++ config.mk
@@ -7,9 +7,15 @@ LIBS = $(ESP_LIBS)/SPI \
   $(ESP_LIBS)/DNSServer \
   $(ESP_LIBS)/ESP8266mDNS \
   $(ARDUINO_LIBS)/WiFiManager
-  
+
+ifeq ($(OS), Windows_NT)
+  # Adjust critical paths
+  UPLOAD_PORT = COM7
+else
+  UPLOAD_PORT = /dev/cu.wchusbserial1420
+endif
 BOARD = nodemcuv2
-UPLOAD_PORT = COM7
+
 UPLOAD_SPEED = 1500000
 FLASH_DEF=4M3M
 #VERBOSE=1
